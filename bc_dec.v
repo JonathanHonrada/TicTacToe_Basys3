f1a0e528efd9ac095884780d12d726a1329c4e32
Jonathan Honrada <45415694+JonathanHonrada@users.noreply.github.com>
2018-11-28 14:46:27 -0800

    Update bc_dec.v

--- bc_dec.v
+++ bc_dec.v
@@ -41,19 +41,6 @@ module BC_DEC(    input CLK,
          cnt_dig <= cnt_dig + 1;
    end 
 
-//   -- select the display sseg data abcdefg (active low) -----
-   /*assign SEGMENTS = 
-                     (digit==0)? 8'b00100101 :  //2
-                     (digit==1)? 8'b01111111 :  //top player 1
-                     (digit==2)? 8'b11111101 :  //middle player 1
-                     (digit==3)? 8'b11101111 :  //bottom player 1
-                     (digit==4)? {disp_clk,7'b1111111} :  //top player 2
-                     (digit==5)? {6'b111111,disp_clk,1'b1} :  //middle player 2
-                     (digit==6)? {3'b111,disp_clk,4'b1111} :  //bottom player 2
-                     (digit==7)? 8'b10011111: // 1
-                     (digit==8)? 8'b11111111:
-                     8'b11111111;*/
-
 //   -- actuate the correct display --------------------------
    assign DISP_EN = (cnt_dig==0)? 4'b1110: 
                     (cnt_dig==1)? 4'b1101:
@@ -76,7 +63,7 @@ module BC_DEC(    input CLK,
 
         2:begin//controls game display for the middle-left seven segment display
          case (pos3)
-            0:seg[4] <= 1;
+	    0:seg[4] <= 1;
             1:seg[4] <= 0;
             2:seg[4] <= disp_clk;
             default: seg[4] <= 1;
